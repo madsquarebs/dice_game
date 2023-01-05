86fe7f7770325e15e8fdc378345406cdd5106fc6
unknown <ankita.jadhav1410@gmail.com>
2023-01-04 19:06:17 -0700

    Added logger to the game

--- sample_project/game.py
+++ sample_project/game.py
@@ -1,22 +1,26 @@
 import random
+import logging
 
 class Game:
+    logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
-        
+
     def roll_dice(self, player):
         dice_value = random.randint(1,6)
-        print(f"{player.get_name()}\'s dice value is {dice_value}")
+        logging.info(f"{player.get_name()}\'s dice value is {dice_value}")
         player.update_score(dice_value)
         return dice_value
         
     def get_winner(self):
-        print("===================================")
-        print(f"{self.player1.get_name()}\'s total dice value is {self.player1.get_score()}")
-        print("===================================")
-        print(f"{self.player2.get_name()}\'s total dice value is {self.player2.get_score()}")
-        print("===================================")
+        logger = logging.getLogger("game")
+        logger.info("===================================")
+        logger.info(f"{self.player1.get_name()}\'s total dice value is {self.player1.get_score()}")
+        logger.info("===================================")
+        logger.info(f"{self.player2.get_name()}\'s total dice value is {self.player2.get_score()}")
+        logger.info("===================================")
         if self.player1.get_score() > self.player2.get_score():
             return "Player1 is the winner!"
         elif self.player2.get_score() > self.player1.get_score():
