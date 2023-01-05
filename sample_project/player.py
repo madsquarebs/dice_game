b5317f49be4ac525cf7dd75e69565b9eaff1bdf6
madsquarebs <ssikmr4755@gmail.com>
2023-01-05 07:29:10 +0530

    Update player.py
    Added logging for player

--- sample_project/player.py
+++ sample_project/player.py
@@ -1,4 +1,8 @@
+import logging
+
 class Player:
+    logging.basicConfig(level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger('Player')
     def __init__(self, name, country, score=0):
         self.name = name
         self.country = country
@@ -6,6 +10,7 @@ class Player:
         
     def update_score(self, score):
         self.score = self.score + score
+        logger.info(f"Updated player score is: {self.score}")
         return True
     
     def get_score(self):
